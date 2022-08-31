4fdf04144952ef725077ad2a1494878a52998598
Stanley Mair <stanleygmair@gmail.com>
2022-09-01 04:52:13 +1200

    Added useLocation hook to add search functionality

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -2,18 +2,14 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectArticles, filterArticles } from "../features/articles/articlesSlice";
 import Search from "./Search";
-import { Link } from "react-router-dom";
-
-// Import Link from React Router
+import { Link, useLocation } from "react-router-dom";
 
 export default function Articles () {
   const articles = useSelector(selectArticles)
   
-  // grab the search value from useLocation()
-  
-  // get the queryParams from new URLSearchParams() 
-  
-  const title = '' // <-- fix me!
+  const {search} = useLocation();
+  const queryParams = new URLSearchParams(search);  
+  const title = queryParams.get('title') 
   
   const filteredArticles = title ? filterArticles(title, articles) : Object.values(articles)
 
